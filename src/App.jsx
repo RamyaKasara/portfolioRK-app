1f63aca1bd121c06de151e44a0d604c75071eac3
ramya.kasarabada@gmail.com <52570872+RamyaKasara@users.noreply.github.com>
2024-11-10 18:36:25 +0530

    Addition of the homepage and navbar files

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HomePage from './Pages/HomePage';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,9 +28,14 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+      <HomePage />
     </>
   )
 }
 
 export default App
+
+// .page{
+// background-color: #303030;
+// }
---
+++ src/pages/homepage/index.jsx
@@ -0,0 +1,14 @@
+import './style.css';
+import Navbar from '../../components/navbar.jsx';
+
+function HomePage() {
+    return ( 
+        <div className='page'>
+            <Navbar/>
+            Home Page
+        </div>
+        
+     );
+}
+
+export default HomePage;
