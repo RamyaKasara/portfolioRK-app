17c57492c0fef33049a82ccf2ad3a1ce71f78dda
RamyaKasarabada <127077423+RamyaKasarabada@users.noreply.github.com>
2025-01-26 17:06:43 +0530

    React not imported error fixed

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    jsxRuntime: 'automatic', // Use the automatic runtime
+  })],
   server: {
     port: 5000 // Change this to your desired port number
   },
